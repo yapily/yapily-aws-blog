83db77ee358f905b3f97af314e044a4ff20d28cd
rafael <rafael.magalhaes@yapily.com>
2022-04-18 20:04:15 +0100

    home page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import Amplify from 'aws-amplify'
-import AmplifyVue from '@aws-amplify/ui-vue';
-import awsconfig from './aws-exports'
-import '@aws-amplify/ui-vue/styles.css';
-Amplify.configure(awsconfig)
-
-createApp(App).use(store).use(router).use(AmplifyVue).mount('#app')
+import Amplify from "aws-amplify";
+import AmplifyVue from "@aws-amplify/ui-vue";
+import awsconfig from "./aws-exports";
+import "@aws-amplify/ui-vue/styles.css";
+Amplify.configure(awsconfig);
+createApp(App).use(store).use(router).use(AmplifyVue).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="container card">
-    <template v-if="institutions && institutions.length">
-      <InstitutionsList :institutions="institutions" />
-    </template>
+  <div class="container">
+    <h1>Select which type of demo</h1>
+    <div class="row d-flex justify-content-around">
+      <div class="col-lg-6">
+        <div class="card m-2 cursor" @click="$router.push('/demo/ais')">
+          <div class="d-flex" id="card-one-title">
+            <h2 class="card-title highlight">Data</h2>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="card m-2 cursor" @click="$router.push('/demo/pis')">
+          <div class="d-flex" id="card-two-title">
+            <h2 class="card-title highlight">Payments</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--    <template v-if="institutions && institutions.length">-->
+    <!--      <InstitutionsList :institutions="institutions" />-->
+    <!--    </template>-->
   </div>
 </template>
 
@@ -11,7 +29,6 @@
 import { toRefs, onMounted, ref } from "vue";
 import { useAuthenticator } from "@aws-amplify/ui-vue";
 import { API } from "aws-amplify";
-import InstitutionsList from "@/components/InstitutionsList.vue";
 const { user } = toRefs(useAuthenticator());
 const institutions = ref([]);
 onMounted(async () => {
@@ -23,3 +40,37 @@ const fetchInstitutions = async () => {
   institutions.value = res.data;
 };
 </script>
+<style lang="scss" scoped>
+h1 {
+  font-size: 80px;
+}
+p {
+  font-size: 20px;
+  font-weight: 400;
+}
+.highlight {
+  color: #0727d3;
+}
+.card-title {
+  width: 100%;
+
+  h2 {
+    text-align: center;
+    font-size: 48px;
+    font-weight: 600;
+  }
+}
+.card {
+  box-shadow: 2px 2px 10px #ccc;
+  cursor: pointer;
+}
+.cards-icons {
+  max-height: 62px;
+  max-width: 62px;
+  min-height: 62px;
+  min-width: 62px;
+}
+.text-content {
+  font-size: 20px;
+}
+</style>
